ffb7c049742b51762c2939b251e132cfa9deabc0
niyazigunduz-rovio <niyazi.gunduz@rovio.com>
2024-11-06 13:54:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ pygame.init()
 # Screen dimensions
 screen_width = 1400
 screen_height = 800
+ground_level = 550
 
 # Colors
 black = (0, 0, 0)
@@ -15,6 +16,7 @@ white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
+dark_green = (0, 200, 0)
 
 # Set up the display
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -40,18 +42,27 @@ def create_boxes():
 
 boxes = create_boxes()
 
-# Physics settings
+# Global gravity setting
 gravity = 0.5
 
 # Button settings
-button_rect = pygame.Rect(700, 50, 80, 30)
+button_width = 150
+button_height = 30
+button_rect = pygame.Rect((screen_width - button_width) // 2, 10, button_width, button_height)
 button_color = green
+button_hover_color = dark_green
 button_text_color = black
 font = pygame.font.Font(None, 36)
+
+# Game over settings
+missed_shots = 0
+max_missed_shots = 3
+game_over = False
 
 # Main game loop
 running = True
 while running:
+    mouse_pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -62,34 +73,51 @@ while running:
                 bird_pos = [150, 450]
                 bird_speed = [0, 0]
                 bird_launched = False
-            elif not bird_launched:
+                mouse_start = None
+                missed_shots = 0
+                game_over = False
+            elif not bird_launched and not game_over:
                 mouse_start = pygame.mouse.get_pos()
-        elif event.type == pygame.MOUSEBUTTONUP and not bird_launched:
+        elif event.type == pygame.MOUSEBUTTONUP and not bird_launched and mouse_start and not game_over:
             mouse_end = pygame.mouse.get_pos()
+            distance = ((mouse_start[0] - mouse_end[0]) ** 2 + (mouse_start[1] - mouse_end[1]) ** 2) ** 0.5
             bird_speed = [(mouse_start[0] - mouse_end[0]) / 10, (mouse_start[1] - mouse_end[1]) / 10]
+            bird_speed = [speed * (distance / 50) for speed in bird_speed]
             bird_launched = True
 
-    if bird_launched:
+    if bird_launched and not game_over:
         bird_speed[1] += gravity
         bird_pos[0] += bird_speed[0]
         bird_pos[1] += bird_speed[1]
 
-    # Check for collisions with boxes
-    bird_rect = pygame.Rect(bird_pos[0], bird_pos[1], 50, 50)
-    for box in boxes:
-        box_rect = pygame.Rect(box[0], box[1], 50, 50)
-        if bird_rect.colliderect(box_rect):
-            boxes.remove(box)
+        # Check for collisions with boxes
+        bird_rect = pygame.Rect(bird_pos[0], bird_pos[1], 50, 50)
+        for box in boxes:
+            box_rect = pygame.Rect(box[0], box[1], 50, 50)
+            if bird_rect.colliderect(box_rect):
+                boxes.remove(box)
+                bird_speed = [0, 0]
+                bird_launched = False
+                bird_pos = [150, 450]
+                mouse_start = None
+
+        # Check for collision with ground
+        if bird_pos[1] >= ground_level:
             bird_speed = [0, 0]
             bird_launched = False
             bird_pos = [150, 450]
+            mouse_start = None
+            missed_shots += 1
+            if missed_shots >= max_missed_shots:
+                game_over = True
 
     # Restart game if all boxes are destroyed
-    if not boxes:
+    if not boxes and not game_over:
         boxes = create_boxes()
         bird_pos = [150, 450]
         bird_speed = [0, 0]
         bird_launched = False
+        mouse_start = None
 
     # Fill the screen with white
     screen.fill(white)
@@ -102,14 +130,23 @@ while running:
         screen.blit(box_image, box)
 
     # Draw the slingshot trajectory
-    if mouse_start and not bird_launched:
+    if mouse_start and not bird_launched and not game_over:
         mouse_current = pygame.mouse.get_pos()
         pygame.draw.line(screen, red, (bird_pos[0] + 25, bird_pos[1] + 25), mouse_current, 5)
 
     # Draw the restart button
-    pygame.draw.rect(screen, button_color, button_rect)
-    button_text = font.render('Restart', True, button_text_color)
+    if button_rect.collidepoint(mouse_pos):
+        pygame.draw.rect(screen, button_hover_color, button_rect)
+    else:
+        pygame.draw.rect(screen, button_color, button_rect)
+    pygame.draw.rect(screen, black, button_rect, 2)
+    button_text = font.render('Restart Game', True, button_text_color)
     screen.blit(button_text, (button_rect.x + 10, button_rect.y + 5))
+
+    # Draw game over text
+    if game_over:
+        game_over_text = font.render('GAME OVER', True, red)
+        screen.blit(game_over_text, (screen_width // 2 - game_over_text.get_width() // 2, screen_height // 2 - game_over_text.get_height() // 2))
 
     # Update the display
     pygame.display.flip()
